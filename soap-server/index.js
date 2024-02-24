0bd170381cabe243f02bb66e2616e00b45119eff
Lochner Louw <137168549+llouw-drs@users.noreply.github.com>
2024-02-24 19:15:47 +1000

    feat: SOAP server

--- soap-server/index.js
+++ soap-server/index.js
@@ -1,3 +1,8 @@
+var http = require('http');
+var soap = require('soap');
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var xml = '<definitions name="HelloService" targetNamespace="http://www.examples.com/wsdl/HelloService.wsdl" xmlns="http://schemas.xmlsoap.org/wsdl/" xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/" xmlns:tns="http://www.examples.com/wsdl/HelloService.wsdl" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><message name="SayHelloRequest"><part name="firstName" type="xsd:string"/></message><message name="SayHelloResponse"><part name="greeting" type="xsd:string"/></message><portType name="Hello_PortType"><operation name="sayHello"><input message="tns:SayHelloRequest"/><output message="tns:SayHelloResponse"/></operation></portType><binding name="Hello_Binding" type="tns:Hello_PortType"><soap:binding style="rpc" transport="http://schemas.xmlsoap.org/soap/http"/><operation name="sayHello"><soap:operation soapAction="sayHello"/><input><soap:body encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" namespace="urn:examples:helloservice" use="encoded"/></input><output><soap:body encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" namespace="urn:examples:helloservice" use="encoded"/></output></operation></binding><service name="Hello_Service"><documentation>WSDL File for HelloService</documentation><port binding="tns:Hello_Binding" name="Hello_Port"><soap:address location="http://localhost:51515/SayHello/" /></port></service></definitions>';
 var service = {
     Hello_Service: {
@@ -20,10 +25,7 @@ var service = {
 //     done();
 //   });
 
-var http = require('http');
-var soap = require('soap');
-var express = require('express');
-var bodyParser = require('body-parser');
+
 
 // //http server example
 // var server = http.createServer(function(request,response) {
@@ -39,7 +41,7 @@ var bodyParser = require('body-parser');
 var app = express();
 //body parser middleware are supported (optional)
 app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
-var server = app.listen(8001, function(){
+var server = app.listen(process.env.PORT || 8001, function(){
     //Note: /wsdl route will be handled by soap module
     //and all other routes & middleware will continue to work
     soap.listen(app, '/SayHello', service, xml, function(){
