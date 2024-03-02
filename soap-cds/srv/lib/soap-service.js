7d4f64bb7f02f4eac5d1c06b662f0d1261f7138c
Lochner Louw <137168549+llouw-drs@users.noreply.github.com>
2024-03-02 14:12:07 +1000

    feat: update soap cds to deploy to cf

--- soap-cds/srv/lib/soap-service.js
+++ soap-cds/srv/lib/soap-service.js
@@ -1,41 +1,69 @@
-const soap = require('soap');
-const client = require('@sap-cloud-sdk/http-client');
-const connectivity = require('@sap-cloud-sdk/connectivity');
+const soap = require("soap");
+const client = require("@sap-cloud-sdk/http-client");
+const connectivity = require("@sap-cloud-sdk/connectivity");
 // const cds = require('@sap/cds');
 
 // Helper to create a soap service through the BTP destination service
 async function getSoapService(service, wsdl, endpoint) {
-    // Get the service reference
-    // var definition = cds.env.requires[service];
-    // console.log(cds);
-    // Get the destination data from BTP
-    // const dest = await connectivity.getDestination({ destinationName: definition.credentials.destination });
-    const dest = await connectivity.getDestination({ destinationName: 'soap-server' });
-    // Get service endpoint
-    const url = (dest.url.substr(dest.url.length-1,1) === '/') ? dest.url.substr(0,dest.url.length-1) : dest.url;
+  // Get the service reference
+  var definition = cds.env.requires[service];
+  var dest;
+  console.log(definition);
+
+  // Get the destination data from BTP
+  if (process.env.NODE_ENV !== "production")
+    dest = await connectivity.getDestination({
+      destinationName: "soap-server",
+    });
+  else
+    dest = await connectivity.getDestination({
+      destinationName: definition.credentials.destination,
+    });
+
+  // Get service endpoint
+  try {
+    const url =
+      dest.url.substr(dest.url.length - 1, 1) === "/"
+        ? dest.url.substr(0, dest.url.length - 1)
+        : dest.url;
     // endpoint.url = url + definition.credentials.path;
-    endpoint.url = url + dest.path;
-
-    // Create an httpClient which connects over the BTP destination
-    var httpClient = {
-        request: async function (url, data, callback, exheaders, exoptions) {
-            client.executeHttpRequest(dest, {
-                method: 'POST',
-                url: url,
-                data: data,
-                headers: exheaders
-            }, {...exoptions, fetchCsrfToken: false}).then((result) => {
-                callback(null, result, result.data);
-            }).catch((e) => {
-                callback(e);
-            });
-        }
+    if (definition.credentials.path) {
+      endpoint.url = url + definition.credentials.path;
+    } else {
+      endpoint.url = url + dest.path;
     }
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 
-    // Instantiate the service using that http client
-    return soap.createClientAsync(wsdl, { httpClient: httpClient });
+  // Create an httpClient which connects over the BTP destination
+  var httpClient = {
+    request: async function (url, data, callback, exheaders, exoptions) {
+      client
+        .executeHttpRequest(
+          dest,
+          {
+            method: "POST",
+            url: url,
+            data: data,
+            headers: exheaders,
+          },
+          { ...exoptions, fetchCsrfToken: false }
+        )
+        .then((result) => {
+          callback(null, result, result.data);
+        })
+        .catch((e) => {
+          callback(e);
+        });
+    },
+  };
+
+  // Instantiate the service using that http client
+  return soap.createClientAsync(wsdl, { httpClient: httpClient });
 }
 
 module.exports = {
-    getSoapService
-}
+  getSoapService,
+};
